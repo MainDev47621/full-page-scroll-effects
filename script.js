d4fbcb687583a2d09ea15e12bba6e01abbb02602
WebDev47621 <webdev47621@gmail.com>
2021-01-17 15:44:34 -0800

    Implement handlebars

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-// const data = {
-//     styling: [
-//         {
-//             bgColor: 'red',
-//             bgImage: 'images/image2.jpg',
-//             bgSize: 'cover',
-//             bgPositionY: '40%',
-//         }
-//     ]
-// }
+const page = document.querySelector('.page');
 
-// const source = document.getElementById('sect').innerHTML;
-// const template = Handlebars.compile(source);
-// const compiledHtml = template(data);
+const data = {
+    styling: [
+        {
+            sectNum: 'sec-1',
+            bgColor: 'red',
+            bgImage: 'images/image2.jpg',
+            bgSize: 'cover',
+            bgPositionY: '40%'
+        }
+    ]
+}
+
+const source = document.getElementById('sect').innerHTML;
+const template = Handlebars.compile(source);
+const compiledHtml = template(data);
+page.innerHTML = compiledHtml;
+
+for (let i of data.styling) {
+    const sec = document.getElementById(i.sectNum);
+    sec.style.backgroundColor = i.bgColor;
+    sec.style.backgroundImage = 'url(' + i.bgImage + ')';
+    sec.style.backgroundSize = i.bgSize;
+    sec.style.backgroundPosition = i.bgPositionY;
+}
