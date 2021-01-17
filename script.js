ff0c899ee303503411ff81985f87b2ba8ba82669
WebDev47621 <webdev47621@gmail.com>
2021-01-17 15:01:47 -0800

    Start setting up Handlebars code

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// const source = document.getElementById('sect');
+// const data = {
+//     styling: [
+//         {
+//             bgColor: 'red',
+//             bgImage: 'images/image2.jpg',
+//             bgSize: 'cover',
+//             bgPositionY: '40%',
+//         }
+//     ]
+// }
+
+// const source = document.getElementById('sect').innerHTML;
 // const template = Handlebars.compile(source);
+// const compiledHtml = template(data);
